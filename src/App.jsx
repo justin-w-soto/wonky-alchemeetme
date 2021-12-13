a361398958e554d13995679c9cab96f808d9155c
Justin W Soto <soto@pdx.edu>
2021-12-13 14:03:46 -0800

    added context folder, and some hooks to get started

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Layout from './views/Layout/Layout'
 import Home from './views/Home/Home'
 import { fetchUser } from './services/user'
 
+
 function App() {
   // inital value should match the data type of end value
   const [user, setUser] = useState({})
---
+++ src/context/UserContext.jsx
@@ -0,0 +1,28 @@
+import { useContext, createContext, useState  }from 'react';
+
+// step 1 create the context
+const UserContext = createContext()
+
+// step 2 create a user provider that takes in children
+const UserProvider = ({ children }) => {
+  const [name, setName] = useState('')
+
+  // step 3 use the .Provider method on our UserContext function and pass children props into the component
+  return <UserContext.Provider value={{ name, setName }}> {children} </UserContext.Provider> 
+}
+
+// steps 4 & 5 create custom hook and pass it the useContext hook and the UserContext function
+// use if statement to define an error 
+ const useCustomHook = () => {
+   const context = useContext(UserContext)
+
+   if (context === undefined) {
+     throw new Error('Error, You have made a mistake.')
+   }
+//    step 6 return ctx
+   return context
+ }
+    // step 7 export UserProvider and useCustomHook
+    // go to index.jsx and wrap app component in Provider component
+export { UserProvider, useCustomHook }
+
